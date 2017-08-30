439b2e471e46af80bb63c0af9ff0ab09fde9ceab
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-30 06:14:27 -0500

    agregamos variables. calculamos y mostramos el resultado

--- PATS-16-6-sumaCuadrado/src/pats/pkg16/pkg6/sumacuadrado/PATS166SumaCuadrado.java
+++ PATS-16-6-sumaCuadrado/src/pats/pkg16/pkg6/sumacuadrado/PATS166SumaCuadrado.java
@@ -5,6 +5,8 @@
  */
 package pats.pkg16.pkg6.sumacuadrado;
 
+import java.util.Scanner;
+
 /**
  *
  * @author edgardo
@@ -16,6 +18,20 @@ public class PATS166SumaCuadrado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        double num1 = 0, num2 = 0, resultado;
+        
+        //Pedimos datos
+        System.out.print("Ingresa el primer numero: ");
+        num1 = teclado.nextDouble();
+        System.out.print("Ingresa el segundo numero: ");
+        num2 = teclado.nextDouble();
+        
+        //Calculamos y mostramos
+        resultado = (Math.pow(num1, 2)) + (Math.pow(num2, 2)) + (2*(num1*num2));
+        System.out.println(resultado);
+        
+        
     }
     
 }
